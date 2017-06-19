a4263d6f04e3a52481eb81a6f34d43230c91ab43
Matt Kruse <github@mattkruse.com>
2017-06-19 15:07:06 -0500

    Final touches on switch from file-based rest to Firebase

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 var express = require('express');
-var simpleFsRest = require('simple-fs-rest');
-
 var app = express();
-
 app.use(express.static('./public_html/'));
-app.use('/games', express.static('./games/'));
-app.use('/lib', express.static('./lib/'));
-app.use('/api',simpleFsRest('rest-data/'));
-
 var server = app.listen(80, function() {
-  console.log('BattleScripts local server is now running!');
+  console.log('BattleScripts local server is now running on port 80!');
 });
