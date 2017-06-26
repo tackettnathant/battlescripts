14f160c291341d13f569a1b2585459b05893c8fd
Nathan Tackett <tackettnathant@gmail.com>
2017-06-25 21:09:04 -0500

    Added auto created_by set to auth uid

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,10 @@
 const functions = require('firebase-functions');
+//const auth = require('firebase-auth');
 
 exports.on_player = functions.database.ref('/players/{id}').onWrite(event => {
+  console.log(JSON.stringify(event));
+  console.log(event.auth.variable.uid);
+
   if (event.data.previous.exists()) {
     // UPDATE
     console.log("Previous value exists, exiting");
@@ -15,5 +19,29 @@ exports.on_player = functions.database.ref('/players/{id}').onWrite(event => {
     // CREATE
     console.log("Writing a generated attribute");
     return event.data.ref.child('created_on').set( (new Date()).getTime() );
+  }
+});
+exports.on_player_created_by = functions.database.ref('/players/{id}').onWrite(event => {
+  var uid;
+  if (event.auth && event.auth.variable.uid){
+    uid=event.auth.variable.uid;
+  } else {
+    uid="admin";
+  }
+
+  if (event.data.previous.exists()) {
+    // UPDATE
+    console.log("Previous value exists, exiting");
+	return;
+  }
+  else if (!event.data.exists()) {
+    // DELETE
+    console.log("Deleting, exiting");
+	return;
+  }
+  else {
+    // CREATE
+    console.log("Writing a generated attribute");
+    return event.data.ref.child('created_by').set(uid);
   }
 });
